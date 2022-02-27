ff0d90d92e9d074eac28089e1aec9bf55aa616ea
Bernard Avenatti <b.avenatti@gmail.com>
2022-02-27 14:45:13 -0600

    Add files via upload

---
+++ Examples/3/CDevelopmentFundamentals/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Single line comment. C++ style supported in C. Looks like C#. :)
+    /* Orignal C multi-line comment. */ 
+    printf("Hello World from /B!");
+    
+    return 0;
+}
